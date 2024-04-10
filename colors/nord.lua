a0df9ec7976aebad3fa942c7bf3dad8c894b03ee
MiguelVeganzones <58487867+MiguelVeganzones@users.noreply.github.com>
2024-04-10 19:55:46 +0200

    Update nord.lua

--- colors/nord.lua
+++ colors/nord.lua
@@ -4,4 +4,27 @@ package.loaded['nord.named_colors'] = nil
 package.loaded['nord.theme'] = nil
 package.loaded['nord.util'] = nil
 
+vim.g.nord_contrast = false 
+vim.g.nord_borders = false
+vim.g.nord_disable_background = false
+vim.g.nord_italic = true
+vim.g.nord_uniform_diff_background = true
+vim.g.nord_bold = true
+
+require("headlines").setup({
+    markdown = {
+        headline_highlights = {
+            "Headline1",
+            "Headline2",
+            "Headline3",
+            "Headline4",
+            "Headline5",
+            "Headline6",
+        },
+        codeblock_highlight = "CodeBlock",
+        dash_highlight = "Dash",
+        quote_highlight = "Quote",
+    },
+})
+
 require('nord').set()
